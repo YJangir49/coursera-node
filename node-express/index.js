0d2fd4e2af5c5da2a8c8ed0ae62161e784b96030
Yogender Jangir <201651054@iiitvadodara.ac.in>
2019-05-31 03:10:19 +0530

    Express with morgan Express static

--- node-express/index.js
+++ node-express/index.js
@@ -1,11 +1,15 @@
 
 const http = require('http');
 const express = require('express');
+const morgan = require('morgan');
 
 const hostname='localhost';
 const port=3000;
 
 const app=express();
+
+app.use(morgan('dev'));
+app.use(express.static(__dirname+'/public'));
 
 app.use((req,res,next)=>{
     console.log(req.headers);
