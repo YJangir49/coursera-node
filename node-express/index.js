9a3dad1ad240d44740051bd29b16a0e7fb88a814
Yogender Jangir <201651054@iiitvadodara.ac.in>
2019-05-31 02:59:14 +0530

    Express server setup Localhost

---
+++ node-express/index.js
@@ -0,0 +1,19 @@
+
+const http = require('http');
+const express = require('express');
+
+const hostname='localhost';
+const port=3000;
+
+const app=express();
+
+app.use((req,res,next)=>{
+    console.log(req.headers);
+    res.statusCode=200;
+    res.setHeader('Content-Type', 'text/html');
+    res.end('<html><body><h5>This is express server</h5></body></html>');
+});
+
+const server = http.createServer(app);
+
+server.listen(port,hostname,()=>{console.log(`Server is running at http://${hostname}:${port}/`);});
