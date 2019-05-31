0bfe1f514d73e819455bbe4e1d809304cd60ee3c
Yogender Jangir <201651054@iiitvadodara.ac.in>
2019-05-31 19:56:40 +0530

    Express Routing with out express router

--- node-express/index.js
+++ node-express/index.js
@@ -3,6 +3,7 @@ const http = require('http');
 const express = require('express');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
+const dishRouter = require('./routes/dishRouter');
 
 const hostname='localhost';
 const port=3000;
@@ -12,49 +13,7 @@ const app=express();
 app.use(bodyParser.json());
 app.use(morgan('dev'));
 app.use(express.static(__dirname+'/public'));
-app.all('/dishes', (req,res,next)=>{
-    res.statusCode=200;
-    res.setHeader('Content-Type', 'text/plain');
-    next();
-});
-
-app.get('/dishes' , (req,res,next)=>{
-    res.end('Will send all dishes to you!');
-});
-
-app.put('/dishes',(req,res,next)=>{
-    res.statusCode=404;
-    res.end('/dishes did not support the PUT operation.');
-});
-
-app.post('/dishes',(req,res,next)=>{
-    res.end('Will add the dish : ' + req.body.name + 'with description ' + req.body.description );
-});
-
-app.delete('/dishes',(req,res,next)=>{
-    res.end('will delete all the dishes for you!');
-});
-
-app.get('/dishes/:dishId', (req,res,next)=>{
-    res.end('Will send you details of dish: ' + req.params.dishId );
-})
-
-
-app.put('/dishes/:dishId', (req,res,next)=>{
-    res.write('Updating the dish: ' + req.params.dishId + '\n' );
-    res.end('Will update the dish: ' + req.body.name + 
-    ' with details: ' + req.body.description);
-})
-
-
-app.post('/dishes/:dishId', (req,res,next)=>{
-    res.end('Post oreration did not supported on /dishes/'+ req.params.dishId );
-})
-
-
-app.delete('/dishes/:dishId', (req,res,next)=>{
-    res.end('Deleting dish:  ' + req.params.dishId );
-})
+app.use('/dishes', dishRouter);
 
 app.use((req,res,next)=>{
     console.log(req.headers);
