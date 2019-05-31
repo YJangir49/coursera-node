463ae65c9228ce041b32100f935ce1a89d75e07b
Yogender Jangir <201651054@iiitvadodara.ac.in>
2019-06-01 00:19:03 +0530

    Assignment 1 Express.Router()

--- node-express/index.js
+++ node-express/index.js
@@ -4,6 +4,8 @@ const express = require('express');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const dishRouter = require('./routes/dishRouter');
+const promoRouter = require('./routes/promoRouter');
+const leaderRouter = require('./routes/leaderRouter');
 
 const hostname='localhost';
 const port=3000;
@@ -14,6 +16,8 @@ app.use(bodyParser.json());
 app.use(morgan('dev'));
 app.use(express.static(__dirname+'/public'));
 app.use('/dishes', dishRouter);
+app.use('/promotions', promoRouter);
+app.use('/leaders', leaderRouter);
 
 app.use((req,res,next)=>{
     console.log(req.headers);
--- node-express/routes/dishRouter.js
+++ node-express/routes/dishRouter.js
@@ -26,5 +26,21 @@ dishRouter.route('/')
     res.end('Deleting all dishes');
 });
 
+dishRouter.route('/:dishId')
+.get((req,res,next)=>{
+    res.end('Will send the dish : '+ req.params.dishId + ' to you!');
+})
+.post((req, res, next) => {
+    res.statusCode = 403;
+    res.end('POST method is not supported on /dishes/' + req.params.dishId);
+})
+.put((req, res, next) => {
+    res.write('Updating Dish: '+ req.params.dishId +'\n');
+    res.end('Will update the dish: '+req.body.name+ ' with description: ' + req.body.description);
+})
+.delete((req, res, next) => {
+    res.end('Deleting dish: ' + req.params.dishId);
+});
+
 module.exports = dishRouter;
 
