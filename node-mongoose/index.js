d2cab58a6c642e30911e51efb59bc95ba3fc4c89
Yogender Jangir <201651054@iiitvadodara.ac.in>
2019-06-03 03:00:29 +0530

    Mongoose with SubSchema/ SubDocuments

--- node-mongoose/index.js
+++ node-mongoose/index.js
@@ -13,10 +13,23 @@ connect.then((db)=>{
     })
     .then((dish)=>{
         console.log(dish);
-        return Dishes.find({});
+        return Dishes.findByIdAndUpdate(dish._id,{
+            $set : { description : 'Updated test'}
+        },{
+            new: true
+        }).exec();
     })
-    .then((dishes)=>{
-        console.log(dishes);
+    .then((dish)=>{
+        console.log(dish);
+        dish.comments.push({
+            rating: 5,
+            comment: 'I\'m getting a sinking feeling!',
+            author: 'Leonardo di Carpaccio'
+        })
+        return dish.save();
+    })
+    .then((dish)=>{
+        console.log(dish);
         return Dishes.remove({});
     })
     .then(()=>{
--- node-mongoose/models/dishes.js
+++ node-mongoose/models/dishes.js
@@ -2,6 +2,25 @@ const mongoose = require('mongoose');
 
 const Schema = mongoose.Schema;
 
+var commentSchema = new Schema({
+    rating:  {
+        type: Number,
+        min: 1,
+        max: 5,
+        required: true
+    },
+    comment:  {
+        type: String,
+        required: true
+    },
+    author:  {
+        type: String,
+        required: true
+    }
+}, {
+    timestamps: true
+});
+
 const dishSchema = new Schema({
     name: {
         type: String,
@@ -12,7 +31,9 @@ const dishSchema = new Schema({
     description: {
         type: String,
         require: true
-    }
+    },
+    
+    comments:[commentSchema]
 },
 {
     timestamp: true
