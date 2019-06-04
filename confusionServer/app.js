fcfd74b994a86b356c120a747e48896ee12dd770
Yogender Jangir <201651054@iiitvadodara.ac.in>
2019-06-04 14:20:24 +0530

    Express Session and Session-file-store

--- confusionServer/app.js
+++ confusionServer/app.js
@@ -3,6 +3,8 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var session = require('express-session');
+var FileStore = require('session-file-store')(session);
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -31,11 +33,20 @@ app.set('view engine', 'jade');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser('12345-67890-09876-54321'));
+//app.use(cookieParser('12345-67890-09876-54321'));
+app.use(session({
+  name: 'session-id',
+  secret : '12345-67890-09876-54321',
+  saveUninitialized: false,
+  resave: false,
+  store : new FileStore()
+}));
 
 function auth (req, res, next) {
+  console.log(req.headers);
+  console.log(req.session);
 
-  if (!req.signedCookies.user) {
+  if (!req.session.user) {
     var authHeader = req.headers.authorization;
     if (!authHeader) {
         var err = new Error('You are not authenticated!');
@@ -48,7 +59,7 @@ function auth (req, res, next) {
     var user = auth[0];
     var pass = auth[1];
     if (user == 'admin' && pass == 'password') {
-        res.cookie('user','admin',{signed: true});
+        req.session.user= 'admin';
         next(); // authorized
     } else {
         var err = new Error('You are not authenticated!');
@@ -58,7 +69,7 @@ function auth (req, res, next) {
     }
   }
   else {
-      if (req.signedCookies.user === 'admin') {
+      if (req.session.user === 'admin') {
           next();
       }
       else {
