289030f4216070b55ae4dd069d2cdc28b254c5f7
Yogender Jangir <201651054@iiitvadodara.ac.in>
2019-06-15 23:36:35 +0530

    Node File Uploading Using Multer

--- confusionServer/app.js
+++ confusionServer/app.js
@@ -11,6 +11,7 @@ var usersRouter = require("./routes/users");
 var dishRouter = require("./routes/dishRouter");
 var promoRouter = require("./routes/promoRouter");
 var leaderRouter = require("./routes/leaderRouter");
+var uploadRouter = require("./routes/uploadRouter");
 
 var passport = require("passport");
 var authenticate = require("./authenticate");
@@ -65,6 +66,7 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use("/dishes", dishRouter);
 app.use("/promotions", promoRouter);
 app.use("/leaders", leaderRouter);
+app.use("/imageUpload", uploadRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
